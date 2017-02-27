6bc49078231192baedf6a068015f5a6ad9dd994d
cwang00 <chao.wang@student.nmt.edu>
2017-02-27 11:52:15 -0700

    Codes added to return name of variables

--- tecplot_dat_info.m
+++ tecplot_dat_info.m
@@ -1,23 +1,32 @@
-function [n_nodes,n_elms,fpst]=tecplot_dat_info(filename)
+function [n_nodes,n_elms,fpst,vars]=tecplot_dat_info(filename)
 %read_tecplot_dat_info Read info from Tecplot importable format *.dat file.
 %   Reture number of nodes and elements, as well as file position.
 %   ChaoWang201512052144
 
 %   ChaoWang201512120853:
 %   Function name changed from read_tecplot_dat_info to tecplot_dat_info
+
+%   ChaoWang201702271110:
+%   Codes added to return name of variables
+
 fid=fopen(filename,'r');
 while 1
     tline=fgetl(fid);
     if strcmp(tline(1:5),'TITLE')
         
     elseif strcmp(tline(1:9),'VARIABLES')
-        
+        var_info = tline;
     elseif strcmp(tline(1:4),'ZONE')
         zone_info=tline;
         break;
     end
 end
+% Get the name of variables
+var_info = var_info(12:end);
+vars = textscan(var_info,'%q','Delimiter',',');
+vars = vars{1};
 
+% Get the number of nodes and elements
 expression=', N=';
 [~,endIndex] = regexp(zone_info,expression);
 n_nodes = sscanf(zone_info(endIndex+1:end), '%g', 1);
